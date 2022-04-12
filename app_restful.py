bd40fcb3cb0e58f33c07e4896ab214bd91213cfd
Ulysses Lopes <ulysses.rlopes@gmail.com>
2022-04-12 19:48:06 -0300

    Incluido requirements do projeto, o app_restful com exemplo de app com extensao flask-restful e incluido o modulo habilidades para gerenciar as habilidades que podem ser associadas ao dev

---
+++ app_restful.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from habilidades import Habilidades
+
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {'nome':'Ulysses',
+     'habilidades':['Python', 'Flask'],
+     },
+    {'nome':'Lopes',
+     'habilidades':['Python', 'Django']}
+]
+
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} nao existe'.format(id)
+            response = {'status': 'ERRO', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o Administrador'
+            response = {'status': 'ERRO', 'mensagem': mensagem}
+        return response
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return dados
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Registro Excluido'}
+
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return desenvolvedores[posicao]
+
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+api.add_resource(Habilidades, '/habilidades/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
